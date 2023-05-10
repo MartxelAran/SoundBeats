97bc8842f9c268a800f4ef751a4ef1a799ece693
MartxelAran <martxel.aranzadi@alumni.mondragon.edu>
2023-05-10 12:37:42 +0200

    L

--- src/main/java/soundbeats/soundbeatsproject/soundbeatsartifact/controllers/LoginController.java
+++ src/main/java/soundbeats/soundbeatsproject/soundbeatsartifact/controllers/LoginController.java
@@ -5,13 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import soundbeats.soundbeatsproject.soundbeatsartifact.domain.usuario.modelo.Usuario;
+import soundbeats.soundbeatsproject.soundbeatsartifact.domain.usuario.repositorio.UserRepository;
+
 @Controller
 public class LoginController {
+
+    UserRepository ur;
     
     @PostMapping("/login")
     public String getLogin(@RequestParam("numSS") String numSS, @RequestParam("1apellido") String apellido,
         @RequestParam("fechadenacimiento") String fechanacimiento, Model model) {
         System.out.println(numSS+" "+apellido+" "+fechanacimiento.toString());
+        Usuario u=ur.findByApellido(apellido);
+        System.out.println(u);
         return "home";
     }
 }
